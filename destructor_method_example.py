ba6e88bdd60865e02299eb199da33d72b98657fa
xuzhigui <xuzhigui1991@163.com>
2017-05-06 16:46:00 +0800

    add something

---
+++ destructor_method_example.py
@@ -0,0 +1,22 @@
+from os.path import join
+
+class FileObject(object):
+    def __init__(self, filepath='~', filename='sample.txt'):
+        self.file = open(join(filepath, filename), 'w')
+
+    def __del__(self):
+        print("close file", self.file)
+        self.file.close()
+        del self.file
+        print("over close file")
+
+a = FileObject(filepath="./")
+a.file.write("abchda\n")
+print(type(a.file))
+print("program over")
+print("program over")
+print("program over")
+print("program over")
+print("program over")
+print("program over")
+print("program over")
