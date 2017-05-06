75299da521b4c5d56f1fcb6bb7506073ee0cc993
xuzhigui <xuzhigui1991@163.com>
2017-05-06 14:12:59 +0800

    change

---
+++ class_example2.py
@@ -0,0 +1,19 @@
+import math
+
+class Structure1(object):
+    _fields = []
+
+    def __init__(self, *args):
+        print args
+        print self._fields
+        if len(args) != len(self._fields):
+            raise TypeError('Expected {} arguments'.format(len(self._fields)))
+
+        for name, value in zip(self._fields, args):
+            setattr(self, name, value)
+
+class Stock(Structure1):
+    _fields = ['name', 'shares', 'price']
+
+s = Stock('ACME', 50, 91.1)
+print s.__dict__
